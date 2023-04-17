8827d365e8b1a45b571379a5e555fdf72a564e34
jose.zamorav <4x1ty.2021@10Jz>
2023-04-17 14:18:46 -0600

    ejercicio de interfaces

--- bases/app.ts
+++ bases/app.ts
@@ -1,27 +1,83 @@
+//Crear interfaces 
+// Cree una interfaz para validar el auto (el valor enviado por paraametro)
+interface Auto{
+    encender:boolean;
+    velocidadMaxima:number;
+    acelerar():void;
+}
 
-//Constantes y let con el tipo de dato
-// const msg:string='Hola mundo'
-// let msg2:string='Hello world'
-// console.log(msg)
-// console.log(msg2)
+const condicirBatimovil=(auto:Auto):void=>{
 
-// const hero={
-//     name:'Ironman',
-//     age:45
-// }
-// hero.age=46
-// console.log(hero.age+9)
-//Tipo de dato number 
-//Nota colocar el tipo para que sea mejor para la traduccion 
+auto.encender=true;
+auto.velocidadMaxima=100;
+auto.acelerar();
+}
 
-
-// (()=>{
-//     const b=10
-//     const a:number=10
-//     let c:number=9;
-//     console.log(a)
-//     console.log(b)
-//     console.log(c)
+const batiMovil:Auto={
+    encender:false,
+    velocidadMaxima:300,
+    acelerar(){
+        console.log(".....¡¡¡¡¡¡¡rrrrrruuuuuuuuuuuuhhhhmmm!!!!!");
+    }
+}
 
 
-// })()
+    //Cree una interfaz con que permita utiilizar el siguiente objeto 
+    //utilizando propiedades opcionales 
+    interface Guason{
+        reir?:boolean;
+        comer?:boolean;
+        llorar?:boolean;
+
+    }
+
+    const guaseon:Guason={
+        reir:true,
+        comer:true,
+        llorar:false
+
+    }
+
+    const reir=(guason:Guason):void=>{
+        if(guaseon.reir){
+            console.log("¡¡¡¡HAHAHAHAHA!!!!");
+            
+        }
+    }
+
+    //Cree una interfaz para la siguiente funcion 
+
+
+    interface fnGothicCity{
+        (citizen:string[]):number
+    }
+    const ciudadGotica:fnGothicCity=(citizen:string[]):number=>{
+        return citizen.length;
+    }
+
+    //Cree una interfaz que oblique a crear una clase con las siguientes propiedades y metodos 
+    interface IPersona{
+        Nombre:string;
+        Edad:number;
+        Sexo:string;
+        EstadoCivil:string;
+        imprimirBio():void;
+    }
+    /**
+     * Propiedades
+     * Nombre
+     * Edad
+     * Sexo
+     * Estado civil
+     * ImprimirBio():void //en consola una breve descripcion
+     */
+    class Persona implements IPersona{
+        Nombre: string;
+        Edad: number;
+        Sexo: string;
+        EstadoCivil: string;
+        imprimirBio(): void {
+           console.log("Descripciom Personita!!!");
+           
+        }
+}
